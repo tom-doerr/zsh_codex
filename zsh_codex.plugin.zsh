ecef53f3b89f29535c79f1c2e8ec6878915a10b3
Tom Dörr <tom.doerr@tum.de>
2022-03-27 09:58:28 +0200

    Switch to insert mode

--- zsh_codex.plugin.zsh
+++ zsh_codex.plugin.zsh
@@ -7,11 +7,16 @@
 create_completion() {
     # Get the text typed until now.
     text=${BUFFER}
-    completion=$(echo -n "$text" | $ZSH_CUSTOM/plugins/zsh_codex/create_completion.py)
+    #echo $cursor_line $cursor_col
+    completion=$(echo -n "$text" | $ZSH_CUSTOM/plugins/zsh_codex/create_completion.py $CURSOR)
+    text_before_cursor=${text:0:$CURSOR}
+    text_after_cursor=${text:$CURSOR}
     # Add completion to the current buffer.
-    BUFFER="${text}${completion}"
-    # Put the cursor at the end of the line.
-    CURSOR=${#BUFFER}
+    #BUFFER="${text}${completion}"
+    BUFFER="${text_before_cursor}${completion}${text_after_cursor}"
+    prefix_and_completion="${text_before_cursor}${completion}"
+    # Put the cursor at the end of the completion
+    CURSOR=${#prefix_and_completion}
 }
 
 # Bind the create_completion function to a key.
