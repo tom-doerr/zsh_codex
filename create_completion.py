67e57a72d95fb87565c24ee30ccd6879493062c1
秋 奈月 <43605695+akinazuki@users.noreply.github.com>
2023-06-24 17:27:37 +0900

    select model via configuration file

--- create_completion.py
+++ create_completion.py
@@ -27,6 +27,7 @@ def create_template_ini_file():
             f.write('[openai]\n')
             f.write('organization_id=\n')
             f.write('secret_key=\n')
+            f.write('model=gpt-3.5-turbo-0613\n')
 
         print('OpenAI API config file created at {}'.format(API_KEYS_LOCATION))
         print('Please edit it and add your organization ID and secret key')
@@ -34,6 +35,7 @@ def create_template_ini_file():
                'need to register for OpenAI Codex: \n'
                 'https://openai.com/blog/openai-codex/')
         sys.exit(1)
+
 
 def initialize_openai_api():
     """
@@ -46,9 +48,10 @@ def initialize_openai_api():
 
     openai.organization_id = config['openai']['organization_id'].strip('"').strip("'")
     openai.api_key = config['openai']['secret_key'].strip('"').strip("'")
+    model = config['openai']['model'].strip('"').strip("'")
+    return model
 
-
-initialize_openai_api()
+model = initialize_openai_api()
 
 cursor_position_char = int(sys.argv[1])
 
@@ -57,7 +60,7 @@ buffer = sys.stdin.read()
 prompt_prefix = '#!/bin/zsh\n\n' + buffer[:cursor_position_char]
 prompt_suffix = buffer[cursor_position_char:]
 full_command = prompt_prefix + prompt_suffix
-response = openai.ChatCompletion.create(model='gpt-3.5-turbo-0613', messages=[
+response = openai.ChatCompletion.create(model=model, messages=[
     {
         "role":'system',
         "content": "you are a zsh shell expert, please help me complete the following command, you should only output the completed command, no need any other include any explain",
