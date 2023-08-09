713f99ca52ddf92eb862e682a6385d339caafc42
Albert <87888006+MustCodeAl@users.noreply.github.com>
2023-08-08 23:56:39 -0500

    Update create_completion.py
    -Added more context from system
    -Enhanced performance

--- create_completion.py
+++ create_completion.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
+import asyncio
 import openai
 import sys
 import os
 import configparser
+import platform
+import functools
+import subprocess
 
 # Get config dir from environment or default to ~/.config
 CONFIG_DIR = os.getenv('XDG_CONFIG_HOME', os.path.expanduser('~/.config'))
 API_KEYS_LOCATION = os.path.join(CONFIG_DIR, 'openaiapirc')
+
 
 # Read the organization_id and secret_key from the ini file ~/.config/openaiapirc
 # The format is:
@@ -17,7 +22,10 @@ API_KEYS_LOCATION = os.path.join(CONFIG_DIR, 'openaiapirc')
 
 # If you don't see your organization ID in the file you can get it from the
 # OpenAI web site: https://openai.com/organizations
-def create_template_ini_file():
+
+
+@functools.lru_cache(maxsize=None)
+async def create_template_ini_file():
     """
     If the ini file does not exist create it and add the organization_id and
     secret_key
@@ -32,17 +40,18 @@ def create_template_ini_file():
         print('OpenAI API config file created at {}'.format(API_KEYS_LOCATION))
         print('Please edit it and add your organization ID and secret key')
         print('If you do not yet have an organization ID and secret key, you\n'
-               'need to register for OpenAI Codex: \n'
-                'https://openai.com/blog/openai-codex/')
+              'need to register for OpenAI Codex: \n'
+              'https://openai.com/blog/openai-codex/')
         sys.exit(1)
 
 
-def initialize_openai_api():
+@functools.lru_cache(maxsize=None)
+async def initialize_openai_api():
     """
     Initialize the OpenAI API
     """
     # Check if file at API_KEYS_LOCATION exists
-    create_template_ini_file()
+    await create_template_ini_file()
     config = configparser.ConfigParser()
     config.read(API_KEYS_LOCATION)
 
@@ -56,10 +65,49 @@ def initialize_openai_api():
 
     return model
 
-model = initialize_openai_api()
+
+# model = initialize_openai_api()
+model = asyncio.run(initialize_openai_api())
 
 cursor_position_char = int(sys.argv[1])
 
+
+@functools.lru_cache(maxsize=None)
+async def get_system_info():
+    """
+    Gather system information
+    """
+    system_info = {
+        "os": platform.system(),
+        "os_version": platform.version(),
+        "architecture": platform.architecture()[0],
+        "processor": platform.processor()
+    }
+    return system_info
+
+
+system_info = asyncio.run(get_system_info())
+
+
+@functools.lru_cache(maxsize=None)
+async def get_installed_packages():
+    # Get list of installed packages
+    try:
+        if system_info['os'] == 'Linux':
+            installed_packages = subprocess.check_output(['dpkg', '--get-selections']).decode('utf-8')
+        elif system_info['os'] == 'Darwin':
+            installed_packages = subprocess.check_output(['brew', 'list']).decode('utf-8')
+        else:
+            installed_packages = "Unsupported OS for package listing"
+    except Exception as e:
+        installed_packages = f"Error getting installed packages: {str(e)}"
+    return installed_packages
+
+
+installed_packages = asyncio.run(get_installed_packages())
+
+# Get current working directory
+current_directory = os.getcwd()
 # Read the input prompt from stdin.
 buffer = sys.stdin.read()
 prompt_prefix = '#!/bin/zsh\n\n' + buffer[:cursor_position_char]
@@ -67,11 +115,15 @@ prompt_suffix = buffer[cursor_position_char:]
 full_command = prompt_prefix + prompt_suffix
 response = openai.ChatCompletion.create(model=model, messages=[
     {
-        "role":'system',
+        "role": 'system',
         "content": "You are a zsh shell expert, please help me complete the following command, you should only output the completed command, no need to include any other explanation",
     },
     {
-        "role":'user',
+        "role": 'user',
+        "content": f"I'm using a {system_info['os']} system with version {system_info['os_version']} on a {system_info['architecture']} architecture with a {system_info['processor']} processor. The current directory is {current_directory}. Here are some of my installed packages:\n{installed_packages}",
+    },
+    {
+        "role": 'user',
         "content": full_command,
     }
 ])
