78d992b6c06ddaa14486ca55b94ff72f9ebd009f
Kukumav <m.fredericW@web.de>
2023-08-22 10:58:12 +0200

    initial commit

--- create_completion.py
+++ create_completion.py
@@ -1,80 +1,28 @@
-#!/usr/bin/env python3
-
-import openai
-import sys
 import os
-import configparser
+import json
+import sys
 
-# Get config dir from environment or default to ~/.config
-CONFIG_DIR = os.getenv('XDG_CONFIG_HOME', os.path.expanduser('~/.config'))
-API_KEYS_LOCATION = os.path.join(CONFIG_DIR, 'openaiapirc')
+conf = None
+with open('config.json') as f:
+    conf = json.load(f)
 
-# Read the organization_id and secret_key from the ini file ~/.config/openaiapirc
-# The format is:
-# [openai]
-# organization_id=<your organization ID>
-# secret_key=<your secret key>
+top_k = conf["top_k"]
+top_p = conf["top_p"]
+temp = conf["temp"]
+n = conf["n"]
+main = conf["path_to_starcoder_cpp_main"]
+model = conf["model_name"]
+system = "<|system|> You are a linux shell expert, please help me complete the following command in the most efficient and shortest way possible. Start your reponse by giving a least detailed step by step explanation for all parts of the shell command. End you response with the completed command. <|end|>:"
+params = f"--top_k {top_k} --top_p {top_p} --temp {temp} -n {n}"
 
-# If you don't see your organization ID in the file you can get it from the
-# OpenAI web site: https://openai.com/organizations
-def create_template_ini_file():
-    """
-    If the ini file does not exist create it and add the organization_id and
-    secret_key
-    """
-    if not os.path.isfile(API_KEYS_LOCATION):
-        with open(API_KEYS_LOCATION, 'w') as f:
-            f.write('[openai]\n')
-            f.write('organization_id=\n')
-            f.write('secret_key=\n')
-            f.write('model=gpt-3.5-turbo-0613\n')
-
-        print('OpenAI API config file created at {}'.format(API_KEYS_LOCATION))
-        print('Please edit it and add your organization ID and secret key')
-        print('If you do not yet have an organization ID and secret key, you\n'
-               'need to register for OpenAI Codex: \n'
-                'https://openai.com/blog/openai-codex/')
-        sys.exit(1)
-
-
-def initialize_openai_api():
-    """
-    Initialize the OpenAI API
-    """
-    # Check if file at API_KEYS_LOCATION exists
-    create_template_ini_file()
-    config = configparser.ConfigParser()
-    config.read(API_KEYS_LOCATION)
-
-    openai.organization_id = config['openai']['organization_id'].strip('"').strip("'")
-    openai.api_key = config['openai']['secret_key'].strip('"').strip("'")
-
-    if 'model' in config['openai']:
-        model = config['openai']['model'].strip('"').strip("'")
-    else:
-        model = 'gpt-3.5-turbo'
-
-    return model
-
-model = initialize_openai_api()
 
 cursor_position_char = int(sys.argv[1])
-
-# Read the input prompt from stdin.
 buffer = sys.stdin.read()
 prompt_prefix = '#!/bin/zsh\n\n' + buffer[:cursor_position_char]
 prompt_suffix = buffer[cursor_position_char:]
 full_command = prompt_prefix + prompt_suffix
-response = openai.ChatCompletion.create(model=model, messages=[
-    {
-        "role":'system',
-        "content": "You are a zsh shell expert, please help me complete the following command, you should only output the completed command, no need to include any other explanation",
-    },
-    {
-        "role":'user',
-        "content": full_command,
-    }
-])
-completed_command = response['choices'][0]['message']['content']
+querry= main + ' -m ' + model + ' -p \"' + system + " " + full_command + " \" " + params
 
-sys.stdout.write(f"\n{completed_command}")
+stream = os.popen(querry)
+output = stream.read()
+sys.stdout.write(f"\n{output}")
