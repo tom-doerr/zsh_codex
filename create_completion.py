48e831da54cae64ca799bf934430eba6badec51a
Bo Li <drluodian@gmail.com>
2023-12-15 11:50:07 +0800

    Processed 1 files and found 0 matches to openai>=1.0.0 interface

--- create_completion.py
+++ create_completion.py
@@ -9,7 +9,6 @@ import configparser
 # Get config dir from environment or default to ~/.config
 CONFIG_DIR = os.getenv('XDG_CONFIG_HOME', os.path.expanduser('~/.config'))
 API_KEYS_LOCATION = os.path.join(CONFIG_DIR, 'openaiapirc')
-client = OpenAI(api_key=CONFIG_DIR['openai']['secret_key'].strip('"').strip("'"))
 # Read the organization_id and secret_key from the ini file ~/.config/openaiapirc
 # The format is:
 # [openai]
@@ -47,18 +46,12 @@ def initialize_openai_api():
     config = configparser.ConfigParser()
     config.read(API_KEYS_LOCATION)
 
-    # TODO: The 'openai.organization_id' option isn't read in the client API. You will need to pass it when you instantiate the client, e.g. 'OpenAI(organization_id=config['openai']['organization_id'].strip('"').strip("'"))'
-    # openai.organization_id = config['openai']['organization_id'].strip('"').strip("'")
+    api_key = config['openai']['secret_key'].strip('"').strip("'")
+    model_name = config['openai'].get('model', 'gpt-3.5-turbo').strip('"').strip("'")
+    client = OpenAI(api_key=api_key)
+    return client, model_name
 
-    if 'model' in config['openai']:
-        model = config['openai']['model'].strip('"').strip("'")
-    else:
-        model = 'gpt-3.5-turbo'
-
-    return model
-
-model = initialize_openai_api()
-
+client, model_name = initialize_openai_api()
 cursor_position_char = int(sys.argv[1])
 
 # Read the input prompt from stdin.
@@ -66,7 +59,7 @@ buffer = sys.stdin.read()
 prompt_prefix = '#!/bin/zsh\n\n' + buffer[:cursor_position_char]
 prompt_suffix = buffer[cursor_position_char:]
 full_command = prompt_prefix + prompt_suffix
-response = client.chat.completions.create(model=model, messages=[
+response = client.chat.completions.create(model=model_name, messages=[
     {
         "role":'system',
         "content": "You are a zsh shell expert, please help me complete the following command, you should only output the completed command, no need to include any other explanation",
@@ -76,6 +69,6 @@ response = client.chat.completions.create(model=model, messages=[
         "content": full_command,
     }
 ])
-completed_command = response['choices'][0]['message']['content']
+completed_command = response.choices[0].message.content
 
 sys.stdout.write(f"\n{completed_command.replace(prompt_prefix, '', 1)}")
