c98f3c15e91e500dc31febdf54df240854f6f9fa
Panchajanya1999 <kernel@panchajanya.dev>
2024-07-05 17:55:18 +0530

    zsh_codex.plugin: Adapt to newer changes
    Signed-off-by: Panchajanya1999 <kernel@panchajanya.dev>

--- zsh_codex.plugin.zsh
+++ zsh_codex.plugin.zsh
@@ -2,23 +2,24 @@
 
 # This ZSH plugin reads the text from the current buffer
 # and uses a Python script to complete the text.
-
+api="openai"
 
 create_completion() {
     # Get the text typed until now.
-    text=${BUFFER}
-    #echo $cursor_line $cursor_col
-    ZSH_CODEX_PYTHON="${ZSH_CODEX_PYTHON:-python3}"
-    completion=$(echo -n "$text" | $ZSH_CODEX_PYTHON $ZSH_CUSTOM/plugins/zsh_codex/create_completion.py $CURSOR)
-    text_before_cursor=${text:0:$CURSOR}
-    text_after_cursor=${text:$CURSOR}
+    local text=$BUFFER
+    local ZSH_CODEX_PYTHON="${ZSH_CODEX_PYTHON:-python3}"
+    local completion=$(echo -n "$text" | $ZSH_CODEX_PYTHON $ZSH_CUSTOM/plugins/zsh_codex/create_completion.py --api "$api" $CURSOR)
+    local text_before_cursor=${BUFFER:0:$CURSOR}
+    local text_after_cursor=${BUFFER:$CURSOR}
+    
     # Add completion to the current buffer.
-    #BUFFER="${text}${completion}"
     BUFFER="${text_before_cursor}${completion}${text_after_cursor}"
-    prefix_and_completion="${text_before_cursor}${completion}"
+    
     # Put the cursor at the end of the completion
-    CURSOR=${#prefix_and_completion}
+    CURSOR=$((CURSOR + ${#completion}))
 }
 
 # Bind the create_completion function to a key.
 zle -N create_completion
+# You may want to add a key binding here, e.g.:
+# bindkey '^X^E' create_completion
